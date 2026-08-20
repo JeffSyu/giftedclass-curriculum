df0fab5c4815ce1bdad07a3d7949d5d6766afff3
JeffSyu <jeffsiu40409@gmail.com>
2026-08-20 17:17:08 +0800

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/giftedclass-curriculum/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
